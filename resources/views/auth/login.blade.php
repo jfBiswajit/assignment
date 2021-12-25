93cc70da2caeaef897dfdae9c3da419e9b2f3548
Biswajit Biswas <jfbiswajit@gmail.com>
2021-12-25 15:13:02 +0600

    Add: Create documentation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,12 +41,12 @@
             </div>
 
             <div class="flex items-center justify-end mt-4">
+              <a style="text-decoration: underline" href="" class="mr-2">Register</a>
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
