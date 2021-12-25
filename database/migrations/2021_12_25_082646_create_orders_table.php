82d77f0666651495c26ec6d7a020b591ded448a9
Biswajit Biswas <jfbiswajit@gmail.com>
2021-12-25 14:29:06 +0600

    Add: migration crated for orders

---
+++ database/migrations/2021_12_25_082646_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('orders', function (Blueprint $table) {
+      $table->id();
+      $table->string('product_id');
+      $table->string('title');
+      $table->string('image_url');
+      $table->longText('description');
+      $table->double('price');
+      $table->timestamps();
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('orders');
+  }
+}
